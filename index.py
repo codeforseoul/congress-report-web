4207b411e3f3a05bed2aef18b92d2e9307e4b794
hoony <thechunsik@gmail.com>
2015-07-09 14:28:21 +0900

    added some comments

--- index.py
+++ index.py
@@ -3,10 +3,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+  # 이메일 신청을 하기 위한 웹페이지
   return render_template('index.html')
 
 @app.route("/signup", methods=['POST'])
 def signup():
+  #
+  # 예지님 코드(form 으로 넘어온 데이터를 디비에 저장)
+  #
   return "hello world!"
 
 
